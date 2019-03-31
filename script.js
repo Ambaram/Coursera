41210c262c249cc078fc1b7d21be44c36a813602
Ambaram <34491921+Ambaram@users.noreply.github.com>
2019-03-31 18:26:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var array=["Yaakov","John","Jen","jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+
+for (var i=0; i <= array.length; i++) {
+		if(array[i].startsWith(("J") || ("j"))){
+			console.log("Hello "+ array[i]);
+		}
+		
+	else{
+		console.log("Goodboy "+ array[i])
+	} 
+		
+}
+
+
+
